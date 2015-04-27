44ace70aa1f323a94ff742e5bb15ae635c61ff46
Laurent TEMPIER <laurent@MacBook-Pro-de-Laurent.local>
2015-04-27 23:13:20 +0200

    Initial Commit

---
+++ LeadXcode/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LeadXcode
+//
+//  Created by Laurent TEMPIER on 27/04/2015.
+//  Copyright (c) 2015 Laurent TEMPIER. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
